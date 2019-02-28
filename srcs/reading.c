27d2a39bfca6499d1e4a912ec703dbc68ade10b4
Allen Melikian <allenwem@gmail.com>
2019-02-28 10:21:27 -0800

    g

--- srcs/reading.c
+++ srcs/reading.c
@@ -1,20 +1,32 @@
 #include "include.h"
 
+void	check_for_h_and_v(t_info *info, char *line)
+{
+	if (line[0] == 'H' || line[0] == 'V')
+	{
+		if (line[0] == 'H')
+			info->input.position = 1;
+		else
+			info->input.position = 0;
+	}
+	else
+		error("Wrong input of horizontal or vertial photo");
+}
+
 void	reading(t_info *info)
 {
 	char	*line;
 	int		i;
 
 	get_next_line(1, &line);
-	check_for_number_of_images();
-	i = 0;
+	check_for_number_of_images(line, info);
+	ft_strdel(line);
 
+	i = 0;
 	while (get_next_line(1, &line) > 0)
 	{
-		if (line[0] == 'H' || line[0] == 'V')
-		{
-			if (line == 0)
-		}
+		check_for_h_and_v(info, line);
+		
 		ft_strdel(line);
 	}
 }
