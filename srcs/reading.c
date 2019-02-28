53af9dfe2e21e99e2f1694fd2e224f31b77c46e0
Allen Melikian <allenwem@gmail.com>
2019-02-28 10:27:08 -0800

    g

--- srcs/reading.c
+++ srcs/reading.c
@@ -1,16 +1,23 @@
 #include "include.h"
 
-void	check_for_h_and_v(t_info *info, char *line)
+void	check_for_h_and_v(t_info *info, int i, char *line)
 {
 	if (line[0] == 'H' || line[0] == 'V')
 	{
 		if (line[0] == 'H')
-			info->input.position = 1;
+			info->input[i]->position = 1;
 		else
-			info->input.position = 0;
+			info->input[i]->position = 0;
 	}
 	else
 		error("Wrong input of horizontal or vertial photo");
+}
+
+void	check_for_correct_photo_pos(t_info *info, int i, char *line)
+{
+	info->index[i]->position = ft_atoi(&line[2]);
+	if (info->index[i]->position < 0 && info->index[i]->position > 100000)
+		error("too many or too few photos");
 }
 
 void	reading(t_info *info)
@@ -25,8 +32,10 @@ void	reading(t_info *info)
 	i = 0;
 	while (get_next_line(1, &line) > 0)
 	{
-		check_for_h_and_v(info, line);
-		
+		check_for_h_and_v(info, i, line);
+		check_for_correct_photo_pos(info, i, line);
+		check_tags(info, i, line);
+		i++;
 		ft_strdel(line);
 	}
 }
